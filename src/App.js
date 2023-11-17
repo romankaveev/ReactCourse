c37d95bd1424b24b740a57bc97d342b92c9a6b97
Roman Kaveev <romankaveev@gmail.com>
2023-11-17 17:56:39 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import './App.css';
+import './Styles/App.css';
 import React, {useState} from "react";
 import Counter from "./Components/Counter";
+import ClassCounter from "./Components/ClassCounter";
+import './Styles/App.css';
 
 function App() {
     const [value, setValue] = useState(" ")
@@ -9,7 +11,17 @@ function App() {
 
     return (
         <div className="App">
-            <Counter/>
+            <div className="post">
+            <div className="post__content">
+            <strong> 1. JS </strong>
+                <div>
+                    Залупный текст для дива
+                </div>
+                <div className="post__btns">
+                    <button> Удалить</button>
+                </div>
+            </div>
+            </div>
         </div>
 
     );
