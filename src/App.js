d089e93777c00fff531aee015691a2eb4a1d68a0
Roman Kaveev <romankaveev@gmail.com>
2023-11-17 15:42:52 +0300

    Counter, ClassCounter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import React, {useState} from "react";
+import Counter from "./Components/Counter";
+
 function App() {
+    const [value, setValue] = useState(" ")
+
+
+
     return (
         <div className="App">
-
+            <Counter/>
         </div>
+
     );
 }
 
