6c36a92ccdf0a55faf66a6157c25fc92755e0487
Roman Kaveev <romankaveev@gmail.com>
2023-11-20 11:33:29 +0300

    add feature for make post

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import React, {useState} from "react";
 import Counter from "./Components/Counter";
 import ClassCounter from "./Components/ClassCounter";
 import './Styles/App.css';
+import PostItem from "./Components/PostItem";
+import PostList from "./Components/PostList";
+import MyButton from "./Components/UI/button/MyButton";
 
 function App() {
-    const [value, setValue] = useState(" ")
-
-
+    const[posts, setPosts] = useState( [
+        {id:1, title: 'JS 1', body: '111'},
+        {id:2, title: 'JS 2', body: '222'},
+        {id:3, title: 'JS 3', body: '333'},
+    ])
 
     return (
         <div className="App">
-            <div className="post">
-            <div className="post__content">
-            <strong> 1. JS </strong>
-                <div>
-                    Залупный текст для дива
-                </div>
-                <div className="post__btns">
-                    <button> Удалить</button>
-                </div>
-            </div>
-            </div>
+            <form>
+                <input type='text' placeholder='Название поста'/>
+                <input type='text' placeholder='Описание поста'/>
+                <MyButton disabled={true} >Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title="JS"/>
         </div>
 
     );
